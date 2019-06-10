e162c5a0b4b9807a9f5ffbc53434fbd580f47acb
Alison Golden <alisong779@yahoo.com>
2019-06-10 18:07:41 +0000

    add view erb files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    set :session_secret, "secret"
     set :public_folder, 'public'
     set :views, 'app/views'
   end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+get '/signup' do
+    if Helpers.is_logged_in?(session)
+      redirect to '/recipes'
+    end
+
+    erb :"/users/create_user"
+  end
+  
+  post '/signup' do
+    if (params[:username]).empty? || (params[:email]).empty? || (params[:password]).empty?
+      redirect to '/signup'
+    end
+
+
+    user = User.create(:username => params["username"], :email => params["email"], :password => params["password"])
+    session[:user_id] = user.id
+
+    redirect to '/recipes'
+  end
