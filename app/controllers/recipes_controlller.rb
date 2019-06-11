636dce0b2504886de41da20670ec7fa23c483b25
Alison Golden <alisong779@yahoo.com>
2019-06-11 18:03:37 +0000

    add user_controller routes

---
+++ app/controllers/recipes_controlller.rb
@@ -0,0 +1,1 @@
+class RecipesController < UsersController
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   
   get '/signup' do
     if Helpers.is_logged_in?(session)
-      redirect to '/recipes'
+      redirect to '/profile'
     end
 
     erb :"/users/create_user"
@@ -20,8 +20,35 @@ class UsersController < ApplicationController
     erb :'users/profile'
   end
   
+  get '/login' do
+    if Helpers.is_logged_in?(session)
+      redirect to '/profile'
+    end
+      erb :"/users/login"
+    end 
+
+  post "/login" do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/profile"
+    else
+      redirect "/login"
+    end
+  end
+
   get '/profile' do 
     erb :'users/profile'
   end 
  
+ get '/logout' do
+    if Helpers.is_logged_in?(session)
+      session.clear
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+
+  
 end 
