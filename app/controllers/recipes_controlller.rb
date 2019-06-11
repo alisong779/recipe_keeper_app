f6af0b9b892db9f305a8bd7b53bbfe4c84018cf9
Alison Golden <alisong779@yahoo.com>
2019-06-11 21:34:12 +0000

    add more recipe routes

--- app/controllers/recipes_controlller.rb
+++ app/controllers/recipes_controlller.rb
@@ -1,10 +1,32 @@
 class RecipesController < ApplicationController
   
-  get '/recipes/create_recipe' do
+  get '/recipes' do
+    if !Helpers.is_logged_in?(session)
+      redirect to '/login'
+    end
+    @recipes = Recipe.all
+    @user = Helpers.current_user(session)
+    erb :"/recipes/recipes"
+  end
+
+get '/recipes/create_recipe' do
     if !Helpers.is_logged_in?(session)
       redirect to '/login'
     end
     erb :"/recipes/create_recipe"
     end
+    
+  post '/recipes' do 
+    user = Helpers.current_user(session)
+    if params[:name].empty?
+      redirect to '/recipes/create_recipe'
+    end
+    @recipe = Recipe.create(name: params[:recipename], ingredients: params[:ingredients], instructions: params[:instructions], :user_id => user.id)
+
+    redirect to '/recipes'
+  end 
+  
+  
+  
 
 end 
