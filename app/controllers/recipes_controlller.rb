bfe2064f23bf752ccdb8290206f79af51ddfb5e7
Alison Golden <alisong779@yahoo.com>
2019-06-12 15:57:28 +0000

    fix errors

--- app/controllers/recipes_controlller.rb
+++ app/controllers/recipes_controlller.rb
@@ -21,7 +21,7 @@ get '/recipes/create_recipe' do
     if params[:name].empty?
       redirect to '/recipes/create_recipe'
     end
-    recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], instructions: params[:instructions], :user_id => @user.id)
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], instructions: params[:instructions], :user_id => @user.id)
 
     redirect to '/recipes'
   end 
